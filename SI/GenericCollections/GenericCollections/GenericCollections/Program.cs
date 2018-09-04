b054c5bf6c1cf175df52c69a888ad10c3719c142
SinyiMark <35994165+SinyiMark@users.noreply.github.com>
2018-09-04 16:54:13 +0200

    si2

---
+++ SI/GenericCollections/GenericCollections/GenericCollections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dictionary<int, string>();
+            d.Add(1, "asd");
+            d.Add(2, "wasd");
+            foreach (var item in d)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
---
+++ SI/LookupCollections/LookupCollections/LookupCollections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ld = new ListDictionary();
+            ld.Add("Estados Unidos", "United States");
+            ld.Add("España", "Spain");
+            ld.Add("Canadá", "Canada");
+            foreach (DictionaryEntry k in ld)
+            {
+                Console.WriteLine($"{k.Key}: {k.Value}");
+            }
+        }
+    }
+}
