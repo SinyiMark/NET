677b2849e4b9c322fb4df5c04bbbfb2137fd4d4a
SinyiMark <35994165+SinyiMark@users.noreply.github.com>
2018-09-06 10:41:54 +0200

    tw 1 copy

---
+++ TW/LinkedList/LinkedList/LinkedList/LinkedLIst.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedLIst
+    {
+        public Node Head { get; set; }
+        public Node Last { get; set; }
+        public int Lenght { get; set; }
+
+        public LinkedLIst()
+        {
+            Head = null;
+            Last = null;
+            Lenght = 0;
+        }
+
+        public void Add(object data)
+        {
+            if (Head == null)
+            {
+                Head = new Node(data);
+                Last = Head;
+            }
+            else
+            {
+                Last.Next = new Node(data);
+            }
+            Lenght++;
+        }
+
+        public object Get(int index) {
+            if (index > Lenght)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Node CurrentNode = Head;
+            for (int i = 0; i < index; i++)
+            {
+                if (i != 0)
+                {
+                    CurrentNode = CurrentNode.Next;
+                }
+            }
+            return CurrentNode.Data;
+        }
+
+        public Node GetNode(int index)
+        {
+            if (index > Lenght)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            Node CurrentNode = Head;
+            for (int i = 0; i < index; i++)
+            {
+                if (i != 0)
+                {
+                    CurrentNode = CurrentNode.Next;
+                }
+            }
+            return CurrentNode;
+        }
+
+        public void Remove(int index) 
+        {
+            Node PreviousNode = GetNode(index - 1);
+            PreviousNode.Next = PreviousNode.Next.Next;
+            Lenght--;
+        }
+
+        public void Insert(int index, object data)
+        {
+            Node PreviousNode = GetNode(index - 1);
+            Node NewNode = new Node(data);
+            NewNode.Next = PreviousNode.Next;
+            PreviousNode.Next = NewNode;
+            Lenght++;
+        }
+    }
+}
---
+++ TW/LinkedList/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedLIst myLinkedLIst = new LinkedLIst();
+            myLinkedLIst.Add(10);
+            myLinkedLIst.Add("dasfsd");
+            Console.WriteLine(myLinkedLIst.Get(2));
+        }
+    }
+}
